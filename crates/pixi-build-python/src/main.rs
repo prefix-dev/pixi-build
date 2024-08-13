b0f93e6b1b6db27d244e5ebe62b6dd81c9dc6f47
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-08-13 16:52:01 +0200

    change to manifest_path

--- crates/pixi-build-python/src/main.rs
+++ crates/pixi-build-python/src/main.rs
@@ -1,12 +1,27 @@
 mod consts;
 
-use std::{collections::BTreeMap, io::BufWriter, path::PathBuf, str::FromStr, sync::Arc};
+use std::{
+    collections::BTreeMap,
+    io::BufWriter,
+    net::SocketAddr,
+    path::{Path, PathBuf},
+    str::FromStr,
+    sync::Arc,
+};
 
 use chrono::Utc;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use clap_verbosity_flag::{InfoLevel, Verbosity};
+use jsonrpc_core::{to_value, Error, IoHandler};
+use jsonrpc_http_server::jsonrpc_core::Params;
 use miette::{Context, IntoDiagnostic};
-use pixi_manifest::{Manifest, SpecType};
+use parking_lot::Mutex;
+use pixi_build_types::{
+    procedures::initialize::{InitializeParams, InitializeResult},
+    BackendCapabilities,
+};
+use pixi_manifest::{Dependencies, Manifest, SpecType};
+use pixi_spec::PixiSpec;
 use rattler_build::{
     build::run_build,
     console_utils::{get_default_env_filter, LoggingOutputHandler},
@@ -20,6 +35,7 @@ use rattler_build::{
 };
 use rattler_conda_types::{
     package::ArchiveType, ChannelConfig, MatchSpec, NoArchType, PackageName, Platform,
+    VersionWithSource,
 };
 use rattler_package_streaming::write::CompressionLevel;
 use reqwest::{Client, Url};
@@ -30,69 +46,30 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 #[derive(Parser)]
 #[clap(version)]
 pub struct App {
-    /// The path to the manifest file
-    #[clap(env, long, env = "PIXI_PROJECT_MANIFEST", default_value = consts::PROJECT_MANIFEST)]
-    manifest_path: PathBuf,
+    #[clap(subcommand)]
+    command: Option<Commands>,
+
+    /// The port to expose the json-rpc server on. If not specified will
+    /// communicate with stdin/stdout.
+    #[clap(long, conflicts_with = "command")]
+    http_port: Option<u16>,
 
     /// Enable verbose logging.
     #[command(flatten)]
     verbose: Verbosity<InfoLevel>,
 }
 
-/// Get the requirements for a default feature
-fn requirements_from_manifest(manifest: &Manifest) -> Requirements {
-    let mut requirements = Requirements::default();
-    let default_features = vec![manifest.default_feature()];
-
-    // Get all different feature types
-    let run_dependencies = default_features
-        .iter()
-        .filter_map(|f| f.dependencies(Some(SpecType::Run), None))
-        .collect::<Vec<_>>();
-    let host_dependencies = default_features
-        .iter()
-        .filter_map(|f| f.dependencies(Some(SpecType::Host), None))
-        .collect::<Vec<_>>();
-    let build_dependencies = default_features
-        .iter()
-        .filter_map(|f| f.dependencies(Some(SpecType::Build), None))
-        .collect::<Vec<_>>();
-
-    requirements.build = build_dependencies
-        .into_iter()
-        .flat_map(|d| d.into_owned().into_iter())
-        .map(|(name, spec)| MatchSpec::from_nameless(spec, Some(name)))
-        .map(Dependency::Spec)
-        .collect();
-
-    requirements.host = host_dependencies
-        .into_iter()
-        .flat_map(|d| d.into_owned().into_iter())
-        .map(|(name, spec)| MatchSpec::from_nameless(spec, Some(name)))
-        .map(Dependency::Spec)
-        .collect();
-
-    requirements.run = run_dependencies
-        .into_iter()
-        .flat_map(|d| d.into_owned().into_iter())
-        .map(|(name, spec)| MatchSpec::from_nameless(spec, Some(name)))
-        .map(Dependency::Spec)
-        .collect();
-
-    // TODO: If the host requirements don't contain pip or python add those
-
-    requirements
-}
-
-fn channels_from_manifest(manifest: &Manifest, channel_config: &ChannelConfig) -> Vec<Url> {
-    // TODO: Improve
-    manifest
-        .parsed
-        .project
-        .channels
-        .iter()
-        .map(|c| c.channel.clone().into_base_url(channel_config))
-        .collect()
+#[derive(Subcommand)]
+pub enum Commands {
+    /// store data as key value pair
+    GetMetadata {
+        #[clap(env, long, env = "PIXI_PROJECT_MANIFEST", default_value = consts::PROJECT_MANIFEST)]
+        manifest_path: PathBuf,
+    },
+    Build {
+        #[clap(env, long, env = "PIXI_PROJECT_MANIFEST", default_value = consts::PROJECT_MANIFEST)]
+        manifest_path: PathBuf,
+    },
 }
 
 #[tokio::main]
@@ -101,6 +78,62 @@ pub async fn main() {
         eprintln!("{err:?}");
         std::process::exit(1);
     }
+}
+
+struct BuildServer {
+    manifest: Manifest,
+    manifest_root: PathBuf,
+}
+
+impl BuildServer {
+    pub fn new(source_dir: &Path) -> miette::Result<Self> {
+        // Load the manifest from the source directory
+        let manifest = Manifest::from_path(&source_dir)
+            .with_context(|| format!("failed to parse manifest from {}", source_dir.display()))?;
+
+        Ok(Self {
+            manifest,
+            manifest_root: source_dir.to_path_buf(),
+        })
+    }
+}
+
+async fn run_server(port: Option<u16>) -> miette::Result<()> {
+    let build_server = Arc::new(Mutex::new(None));
+
+    // Construct a server
+    let mut io = IoHandler::new();
+    io.add_sync_method("initialize", move |params: Params| {
+        let params: InitializeParams = params.parse()?;
+
+        let mut build_server = build_server.lock();
+        if build_server.is_some() {
+            return Err(Error::invalid_request());
+        }
+
+        *build_server = Some(
+            BuildServer::new(&params.source_dir)
+                .map_err(|e| Error::invalid_params(e.to_string()))?,
+        );
+
+        Ok(to_value(InitializeResult {
+            capabilities: BackendCapabilities {
+                provides_conda_metadata: Some(true),
+            },
+        })
+        .expect("failed to convert to json"))
+    });
+
+    if let Some(port) = port {
+        jsonrpc_http_server::ServerBuilder::new(io)
+            .start_http(&SocketAddr::from(([127, 0, 0, 1], port)))
+            .into_diagnostic()?
+            .wait()
+    } else {
+        jsonrpc_stdio_server::ServerBuilder::new(io).build().await;
+    }
+
+    Ok(())
 }
 
 async fn actual_main() -> miette::Result<()> {
@@ -112,15 +145,21 @@ async fn actual_main() -> miette::Result<()> {
         .with(get_default_env_filter(args.verbose.log_level_filter()).into_diagnostic()?);
     registry.with(log_handler.clone()).init();
 
+    match args.command {
+        None => run_server(args.http_port).await,
+        Some(Commands::Build { manifest_path }) => build(log_handler, &manifest_path).await,
+        Some(Commands::GetMetadata { .. }) => unimplemented!(),
+    }
+}
+
+async fn build(
+    logging_output_handler: LoggingOutputHandler,
+    manifest_path: &Path,
+) -> miette::Result<()> {
     // Load the manifest
-    let manifest = Manifest::from_path(&args.manifest_path).with_context(|| {
-        format!(
-            "failed to parse manifest from {}",
-            args.manifest_path.display()
-        )
-    })?;
-    let manifest_root = args
-        .manifest_path
+    let manifest = Manifest::from_path(&manifest_path)
+        .with_context(|| format!("failed to parse manifest from {}", manifest_path.display()))?;
+    let manifest_root = manifest_path
         .parent()
         .expect("the project manifest must reside in a directory");
 
@@ -150,7 +189,7 @@ async fn actual_main() -> miette::Result<()> {
         .context("failed to create output directory")?;
     let directories = Directories::setup(
         name.as_normalized(),
-        args.manifest_path.as_path(),
+        manifest_path,
         output_dir.path(),
         false,
         &Utc::now(),
@@ -161,7 +200,7 @@ async fn actual_main() -> miette::Result<()> {
     // TODO: Read from config / project.
     let channel_config = ChannelConfig::default_with_root_dir(manifest_root.to_path_buf());
 
-    let requirements = requirements_from_manifest(&manifest);
+    let requirements = requirements_from_manifest(&manifest, &channel_config);
     let channels = channels_from_manifest(&manifest, &channel_config);
 
     let host_platform = Platform::current();
@@ -175,7 +214,7 @@ async fn actual_main() -> miette::Result<()> {
         recipe: Recipe {
             schema_version: 1,
             package: Package {
-                version: version.into(),
+                version: VersionWithSource::from(version),
                 name,
             },
             cache: None,
@@ -202,7 +241,7 @@ async fn actual_main() -> miette::Result<()> {
                     } else {
                         vec!["$PYTHON -m pip install --ignore-installed --no-deps --no-build-isolation . -vv".to_string()]
                     })
-                .into(),
+                    .into(),
                 noarch: noarch_type,
 
                 // TODO: Python is not exposed properly
@@ -251,7 +290,7 @@ async fn actual_main() -> miette::Result<()> {
     };
 
     let tool_config = Configuration {
-        fancy_log_handler: log_handler,
+        fancy_log_handler: logging_output_handler,
         client: reqwest_middleware::ClientWithMiddleware::from(Client::default()),
         no_clean: false,
         no_test: false,
@@ -285,4 +324,68 @@ async fn actual_main() -> miette::Result<()> {
         .context("failed to remove temporary recipe file")?;
 
     Ok(())
+}
+
+/// Get the requirements for a default feature
+fn requirements_from_manifest(manifest: &Manifest, channel_config: &ChannelConfig) -> Requirements {
+    let mut requirements = Requirements::default();
+    let default_features = vec![manifest.default_feature()];
+
+    // Get all different feature types
+    let run_dependencies = Dependencies::from(
+        default_features
+            .iter()
+            .filter_map(|f| f.dependencies(Some(SpecType::Run), None)),
+    );
+    let host_dependencies = Dependencies::from(
+        default_features
+            .iter()
+            .filter_map(|f| f.dependencies(Some(SpecType::Host), None)),
+    );
+    let build_dependencies = Dependencies::from(
+        default_features
+            .iter()
+            .filter_map(|f| f.dependencies(Some(SpecType::Build), None)),
+    );
+
+    requirements.build = dependencies_into_matchspecs(build_dependencies, channel_config)
+        .into_iter()
+        .map(Dependency::Spec)
+        .collect();
+    requirements.host = dependencies_into_matchspecs(host_dependencies, channel_config)
+        .into_iter()
+        .map(Dependency::Spec)
+        .collect();
+    requirements.run = dependencies_into_matchspecs(run_dependencies, channel_config)
+        .into_iter()
+        .map(Dependency::Spec)
+        .collect();
+
+    // TODO: If the host requirements don't contain pip or python add those
+
+    requirements
+}
+
+fn channels_from_manifest(manifest: &Manifest, channel_config: &ChannelConfig) -> Vec<Url> {
+    // TODO: Improve
+    manifest
+        .parsed
+        .project
+        .channels
+        .iter()
+        .map(|c| c.channel.clone().into_base_url(channel_config))
+        .collect()
+}
+
+fn dependencies_into_matchspecs(
+    deps: Dependencies<PackageName, PixiSpec>,
+    channel_config: &ChannelConfig,
+) -> Vec<MatchSpec> {
+    deps.into_specs()
+        .filter_map(|(name, spec)| {
+            spec.try_into_nameless_match_spec(channel_config)
+                .expect("failed to convert spec into match spec")
+                .map(|spec| MatchSpec::from_nameless(spec, Some(name)))
+        })
+        .collect()
 }
