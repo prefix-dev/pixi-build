b7f38c5347270b7ff22daca07837fdbbd5d1a3ff
Tim de Jager <tim@prefix.dev>
2024-09-20 09:38:14 +0200

    fix: try to fix ci again

---
+++ intra-doc-links.bash
@@ -0,0 +1,21 @@
+cargo metadata --no-deps --format-version=1 \
+| jq -r '.packages[] | .name as $pname | .targets[] | [$pname, .kind[], .name] | @tsv' \
+| while IFS=$'\t' read -r package kind name; do
+    case "$kind" in
+        lib)
+            cargo rustdoc -p "$package" --lib --all-features -- -D warnings -W unreachable-pub
+            ;;
+        bin)
+            cargo rustdoc -p "$package" --bin "$name" --all-features -- -D warnings -W unreachable-pub
+            ;;
+        example)
+            cargo rustdoc -p "$package" --example "$name" --all-features -- -D warnings -W unreachable-pub
+            ;;
+        test)
+            cargo rustdoc -p "$package" --test "$name" --all-features -- -D warnings -W unreachable-pub
+            ;;
+        bench)
+            cargo rustdoc -p "$package" --bench "$name" --all-features -- -D warnings -W unreachable-pub
+            ;;
+    esac
+done
